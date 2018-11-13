e45de506c6ffd69ec969e511c3d472326a415b12
EC2 Default User <ec2-user@ip-172-31-13-179.us-east-2.compute.internal>
2018-11-13 06:47:42 +0000

    Adding Q1 and Q2

---
+++ Q1.c
@@ -0,0 +1,43 @@
+
+#include <stdio.h>
+
+main() {
+
+   int count = 0;  // count how mach number i get.
+   int sum = 0; // sum all the nums.
+   int num; // the numer from the user
+   double average = 0;
+   int pure;
+   
+   printf("Welcome, please enter the weights of each ingredient:\n");
+   scanf("%d", &num); // scaning the first num.
+   sum =sum +num; // sum the first num.
+      
+   while (num >= 0){ // while the user didnt push -(num) count++ and sum the num.
+      count++;
+      scanf("%d", &num);
+      sum =sum+num;
+   }
+   if (num <= -1){ // if he put a negative number i remove heis value from the sum.
+      sum = sum - num;
+   }
+      if (count <3){ // if there is low then 3 nums.
+         printf("Not enough ingredients.\n");
+         return 0;
+      }
+      if (count >10){ // if there is more them 10 nums.
+         printf("Too many ingredients.\n");
+         return 0;
+      }
+      printf("the count is : %d\n",count);
+      printf("the sum is: %d",sum);
+      average = (double)sum/count; //calculate the average of the numbers.
+      pure = count*10; // claculate the prercent of the pure.
+
+   printf("The final product weighs %.3f pounds and is %d percent pure.\n",average , pure);
+
+   return 0;
+}  
+ 
+
+ 
---
+++ Q2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+main() {
+    
+    int count = 0;
+    char c; //the char from the user
+    char value; 
+    printf("Enter Char to Count:");
+    c = getchar(); // get the first char.
+
+    
+    if(c != 'm' && c != 'M' && c != 's' & c != 'S'){ //if the user dont give a right char.
+        return 0;
+    }
+        value = getchar();
+        while (value != '$'){ // if the char != $ keep scan chars.
+          value = getchar();
+          if(value == c){ //count how much from the char.
+          count++;
+          }
+        }
+     if (count > 0){   
+    printf("the Char with ASCII code is : %d appeard: %d", c, count);    // print the ascii value and the count;
+     }
+    
+    return 0;
+}
